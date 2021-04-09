477283bbd7ba7e1f51f378796dbe07aef83f4a6b
NoahDLP <81577353+NoahDLP@users.noreply.github.com>
2021-04-09 08:59:56 -0700

    Update lab1a.py

--- lab1a.py
+++ lab1a.py
@@ -16,16 +16,38 @@ from typing import List
 def max_list_iter(int_list: Optional[List]) -> Optional[int]:
    """finds the max of a list of numbers and returns the value (not the index)
    If int_list is empty, returns None. If list is None, raises ValueError"""
-   pass
+
+   if int_list is None:
+      raise ValueError
+   elif len(int_list) == 0:
+      return None
+   else:
+      max = int_list[0]
+      for i in int_list:
+         if i > max:
+            max = i
+      return max
 
 # Maybe_List -> Maybe_List
 def reverse_list(int_list: Optional[List]) -> Optional[List]:
    """reverses a list of numbers and returns the reversed list
    If list is None, raises ValueError"""
-   pass
+
+
+   if int_list is None:
+      raise ValueError
+   else:
+      revLst = int_list[::-1]
+      return revLst
+
+
 
 # Maybe_List -> None
 def reverse_list_mutate(int_list: Optional[List]) -> None:
    """reverses a list of numbers, modifying the input list, returns None
    If list is None, raises ValueError"""
-   pass
+
+   if int_list is None:
+      raise ValueError
+   else:
+      int_list[:] = reverse_list(int_list)
